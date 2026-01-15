1e6d80137d8fb26f985b48f6b5b02512b897e981
IgorAleksejevTTHK <igoralekseje@gmail.com>
2026-01-15 16:07:32 +0200

    Comment out sensitive database credentials
    Comment out database connection details for security.

--- valimised_login/config.php
+++ valimised_login/config.php
@@ -8,8 +8,8 @@ $yhendus=new mysqli($serverinimi,$kasutajanimi,$parool,$andmebaasinimi);
 $yhendus->set_charset('utf8');
 
 //$serverinimi='d141126.mysql.zonevs.eu';
-//$kasutajanimi='d141126_igor';
-//$parool='1448ff12Jpa';
+//$kasutajanimi='';
+//$parool='nottoday';
 //$andmebaasinimi='d141126_baasphp';
 //$yhendus=new mysqli($serverinimi,$kasutajanimi,$parool,$andmebaasinimi);
 //$yhendus->set_charset('utf8');
